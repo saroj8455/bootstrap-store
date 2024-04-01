1f5f61d07d36ca84fc6a8b46c18027adb399a9d8
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2024-04-02 00:43:15 +0530

    update with basic component

--- angular-bootstrap-store/src/app/app.module.ts
+++ angular-bootstrap-store/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AppPageComponent } from './components/app-page/app-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    AppPageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
